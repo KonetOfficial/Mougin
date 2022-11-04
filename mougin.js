5b95be2b916f329092137dd5916785a5a86820fb
Konet <107544026+KonetOfficial@users.noreply.github.com>
2022-11-04 17:54:43 +0100

    Ajout de la liste de tout les animes

--- mougin.js
+++ mougin.js
@@ -13,14 +13,21 @@ async function anime(name) {
         }
     }).catch(e => {
         console.error('Une erreur est survenue', e)
-    });/*
-    .then(data => {
-        console.log(data)
-      const bo = data.data
-      return bo
+    }); 
+}
+async function list() {
+  return fetch(`https://konet-anime-db.normioffi.repl.co/all`,{
+    headers: {
+        Accept: 'application/json'
+    }
+}).then(r => {
+        if (r.ok) {
+            return r.json()
+        } else {
+            throw new Error('Erreur du serveur: ', {cause: r})
+        }
     }).catch(e => {
         console.error('Une erreur est survenue', e)
-    });*/ 
+    }); 
 }
-
-module.exports = {anime};
+module.exports = {anime, list};
